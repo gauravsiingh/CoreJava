a3760e39b8a8ee83bce00c9733291ef87de4f347
Gaurav <kgaurav9singh@gmail.com>
2019-04-14 22:56:39 +0530

    Thread communication demo

---
+++ src/com/gaurav/javafromnotes/threads/threadinteraction/CommunicationDemo1.java
@@ -0,0 +1,64 @@
+package com.gaurav.javafromnotes.threads.threadinteraction;
+
+class Producer1 implements Runnable {
+	StringBuffer sb;
+	boolean dpo;
+
+	Producer1() {
+		sb = new StringBuffer();
+	}
+
+	@Override
+	public void run() {
+		for (int i = 1; i <= 10; i++) {
+			
+			try {
+				sb.append(i + " : ");
+				Thread.sleep(100);
+				System.out.println("Appending : " + i);
+			} catch (InterruptedException e) {
+			}
+
+			dpo = true;
+
+		}
+	}
+
+}
+
+class Consumer1 implements Runnable {
+	Producer1 producer1;
+
+	Consumer1(Producer1 producer1) {
+		this.producer1 = producer1;
+	}
+
+	@Override
+	public void run() {
+		while (!producer1.dpo) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+			}
+
+			System.out.println("producer1.sb = " + producer1.sb);
+		}
+	}
+
+}
+
+public class CommunicationDemo1 {
+
+	public static void main(String[] args) {
+		Producer1 producer1 = new Producer1();
+		Consumer1 consumer1 = new Consumer1(producer1);
+
+		Thread thread1 = new Thread(producer1);
+		Thread thread2 = new Thread(consumer1);
+		
+		thread1.start();
+		thread2.start();
+
+	}
+
+}
---
+++ src/com/gaurav/javafromnotes/threads/threadinteraction/CommunicationDemo2.java
@@ -0,0 +1,64 @@
+package com.gaurav.javafromnotes.threads.threadinteraction;
+
+class Producer2 implements Runnable {
+	StringBuffer sb;
+	boolean dpo;
+
+	Producer2() {
+		sb = new StringBuffer();
+	}
+
+	@Override
+	public void run() {
+		synchronized (sb) {
+			for (int i = 1; i <= 10; i++) {
+				try {
+					sb.append(i + " : ");
+					Thread.sleep(100);
+					System.out.println("Appending : " + i);
+				} catch (InterruptedException e) {
+				}
+			}
+			sb.notify();
+		}
+
+	}
+
+}
+
+class Consumer2 implements Runnable {
+	Producer2 producer2;
+
+	Consumer2(Producer2 Producer2) {
+		this.producer2 = Producer2;
+	}
+
+	@Override
+	public void run() {
+		synchronized (producer2.sb) {
+			try {
+				producer2.sb.wait();
+			} catch (InterruptedException e) {}
+
+			System.out.println("producer2.sb = " + producer2.sb);
+		}
+
+	}
+
+}
+
+public class CommunicationDemo2 {
+
+	public static void main(String[] args) {
+		Producer2 producer2 = new Producer2();
+		Consumer2 consumer2 = new Consumer2(producer2);
+
+		Thread thread1 = new Thread(producer2);
+		Thread thread2 = new Thread(consumer2);
+
+		thread1.start();
+		thread2.start();
+
+	}
+
+}
